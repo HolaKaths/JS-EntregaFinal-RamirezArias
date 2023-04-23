5853d710af4d5ddfa94b575e6dc3a4aed0507cce
Katheryn Ramirez <holakaths@gmail.com>
2023-04-23 16:57:51 +0200

    Correcciones API

--- js/randompet.js
+++ js/randompet.js
@@ -1,38 +1,34 @@
+//Random pet
 
-//RANDOM PET
+
+
+const imageContainer = document.getElementById('image-container');
+
 function getRandomDoggo() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(checkStatus)
-        .then(response => response.json())
-        .then(data => handleData(data))
-        .catch(error => notifyUser(error))
-}
-
-randomButton.addEventListener('click', getRandomDoggo);
-
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
+  fetch('https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1', {
+    headers: {
+      'Authorization': 'Bearer live_Ev2yojB7VRgNtCZETq8pvDYq0jynQIGdBFnHAb1pCIctVUwOBJc1PHeFt1HIiRKL',
+      'Content-Type': 'application/json'
     }
+  })
+    .then(response => response.json())
+    .then(data => handleData(data))
+    .catch(error => notifyUser(error))
 }
 
 function handleData(data) {
-    let url = data.message;
-    console.log(url)
-    let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
-    let breedName = regex.exec(url);
-    document.getElementById('randomImageContainer').innerHTML = `<img alt="random image of a ${fixBreed(breedName[1])}" src='${url}'/>`;
-    document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${fixBreed(breedName[1])}</p>`;
+  let url = data[0].url;
+  let breedName = data[0].breeds[0].name;
+  imageContainer.innerHTML = `<img alt="random image of a ${breedName}" src='${url}'/>`;
+  document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${breedName}</p>`;
 }
 
 function notifyUser(error) {
-    const errorContainer = document.querySelector('.alert');
-    errorContainer.innerHTML = `There was an error with the server request (${error}). <br> Click the button again.`;
-    errorContainer.style.display = 'block';
-    setTimeout(() => {
-        errorContainer.innerHTML = '';
-        errorContainer.style.display = 'none';
-    }, 4000)
+  const errorContainer = document.querySelector('.alert');
+  errorContainer.innerHTML = `There was an error with the server request (${error}). <br> Click the button again.`;
+  errorContainer.style.display = 'block';
+  setTimeout(() => {
+    errorContainer.innerHTML = '';
+    errorContainer.style.display = 'none';
+  }, 4000)
 }
