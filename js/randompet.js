0f63a110a0a4881722478483a798eb4e9f264a86
Katheryn Ramirez <holakaths@gmail.com>
2023-04-25 08:55:23 +0200

    Optimizaciones

--- js/randompet.js
+++ js/randompet.js
@@ -1,36 +1,28 @@
-//Random pet
-
-const imageContainer = document.getElementById('image-container');
-
-function getRandomDoggo() {
-    fetch('https://api.thecatapi.com/v1/images/search', {
-        headers: {
-            'Authorization': 'Bearer live_Ev2yojB7VRgNtCZETq8pvDYq0jynQIGdBFnHAb1pCIctVUwOBJc1PHeFt1HIiRKL',
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => handleData(data))
-        .catch(error => notifyUser(error))
-}
-
-function handleData(data) {
-    if (data && data.length > 0 && data[0].breeds && data[0].breeds.length > 0) {
-        let breedName = data[0].breeds[0].name;
-        let url = data[0].url;
-        imageContainer.innerHTML = `<img alt="random image of a ${breedName}" src='${url}'/>`;
-        document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${breedName}</p>`;
-    } else {
-        notifyUser('No se encontró ninguna imagen de perro.');
+function obtenerRazaAleatoria() {
+    const randomImageContainer = document.getElementById("randomImageContainer");
+    const existingImage = randomImageContainer.querySelector("img");
+    if (existingImage) {
+      existingImage.remove();
     }
-}
-
-function notifyUser(error) {
-    const errorContainer = document.querySelector('.alert');
-    errorContainer.innerHTML = `There was an error with the server request (${error}). <br> Click the button again.`;
-    errorContainer.style.display = 'block';
-    setTimeout(() => {
-        errorContainer.innerHTML = '';
-        errorContainer.style.display = 'none';
-    }, 4000)
-}
+  
+    const alert = document.getElementById("alert");
+  
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error al cargar la imagen");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const img = document.createElement("img");
+        img.src = data.message;
+        randomImageContainer.appendChild(img);
+        alert.style.display = "none";
+      })
+      .catch(error => {
+        console.error(error);
+        alert.style.display = "block";
+        alert.textContent = error.message;
+      });
+  }
